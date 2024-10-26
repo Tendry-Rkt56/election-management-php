ff90cb009e24a21f4b30e61ceefdebdd15770b17
Tendry <tendryzephyrin@gmail.com>
2024-10-26 11:17:26 +0200

    Modification du BureauControleur

--- src/Controller/BureauController.php
+++ src/Controller/BureauController.php
@@ -4,14 +4,12 @@ namespace App\Controller;
 
 use App\Manager;
 
-class BureauController extends Controller {
+class BureauController extends Controller
+{
 
     public function editBureau ($id)
     {
         $bureau = Manager::getManager()->getEntity('bureau')->getBureau($id);
-        // $centres = App::getManager()->getEntity('centre')->getAllCentreVote();
-        // var_dump($bureau,$centres);
-        // die();
         return $this->view('admin.editBureau', compact('bureau', 'centres'));
     }
 
